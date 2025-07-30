17f98b016f78e95e48cd10b24f9a2260086c702d
irischoy <irischm@outlook.com>
2025-07-30 17:39:18 -0400

    uml

--- player.h
+++ player.h
@@ -6,29 +6,31 @@
 #include <set>
 #include "link.h"
 #include "ability.h"
+using namespace std;
+
 class Board; 
 class Player {
-    std::string name;
-    std::vector<std::unique_ptr<Link>>    links;
-    std::vector<std::unique_ptr<Ability>> abilities;
-    std::set<char> revealedOpponentLinks;
+    string name;
+    vector<unique_ptr<Link>>    links;
+    vector<unique_ptr<Ability>> abilities;
+    set<char> revealedOpponentLinks;
     int dataDownloaded    = 0;
     int virusDownloaded   = 0;
     int abilityUsesLeft   = 5;
 
 public:
-    explicit Player(const std::string& name);
+    explicit Player(const string& name);
     void addLink(Link* link);
     Link* getLinkById(char id) const;
-    const std::vector<std::unique_ptr<Link>>& getLinks() const;
-    std::vector<std::unique_ptr<Link>>& getLinks();
-    void addAbility(std::unique_ptr<Ability> ability);
-    std::vector<std::unique_ptr<Ability>>& getAbilities();
-    void useAbility(int abilityIndex, const std::vector<std::string>& args, Player& opponent);
+    const vector<unique_ptr<Link>>& getLinks() const;
+    vector<unique_ptr<Link>>& getLinks();
+    void addAbility(unique_ptr<Ability> ability);
+    vector<unique_ptr<Ability>>& getAbilities();
+    void useAbility(int abilityIndex, const vector<string>& args, Player& opponent);
     void downloadLink(Link* link);
     int  getDataDownloaded()  const;
     int  getVirusDownloaded() const;
-    const std::string& getName() const;
+    const string& getName() const;
     int getAbilityUsesLeft() const;
     void printStatus(const Player& viewer) const;
     void revealLink(char linkId);
